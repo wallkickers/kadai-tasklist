1d269fa852708c9397c82bb4b183b25dff6ede4d
takafumi.m <taka.youthful.426@gmail.com>
2018-05-10 10:34:41 +0000

    user login

--- Gemfile
+++ Gemfile
@@ -30,7 +30,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -57,3 +57,4 @@ gem 'pry-byebug', group: :development
 group :production do
   gem 'pg', '0.21.0'
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get 'toppages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,5 +12,13 @@ Rails.application.routes.draw do
   
   root to: "toppages#index"
   
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  resources :users
+  get "signup",to: "users#new"
+  
   resources :tasks
+  
 end
